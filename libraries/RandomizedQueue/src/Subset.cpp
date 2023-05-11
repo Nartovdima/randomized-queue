54504a7a76cecdf712a69d7ce37d298c10968bfa
Nartov Dima <nartov.dima2018@yandex.ru>
2023-05-11 17:37:57 +0300

    Added Queue realization

--- libraries/RandomizedQueue/src/Subset.cpp
+++ libraries/RandomizedQueue/src/Subset.cpp
@@ -2,10 +2,19 @@
 
 #include "RandomizedQueue.hpp"
 
+#include <string>
+
 void subset(unsigned long k, std::istream& in, std::ostream& out) {
+    RandomizedQueue<std::string> queue;
     std::string line;
-    while (k > 0 && std::getline(in, line)) {
-        out << line << std::endl;
-        --k;
+    while (std::getline(in, line)) {
+        queue.enqueue(line);
+    }
+    
+    auto it = queue.cbegin();
+    auto q_end = queue.cend();
+    while (k-- && it != q_end) {
+        out << *it << std::endl;
+        ++it;
     }
 }
